c13e165394298fc7868a6e512c835ba97b759b0f
Santiago Serrano <santiagoandressp@ufps.edu.co>
2022-04-07 21:05:02 -0500

    Modificacion en los tamaños de la bdd

--- src/main/java/ufps/ahp/security/model/Usuario.java
+++ src/main/java/ufps/ahp/security/model/Usuario.java
@@ -27,12 +27,12 @@ public class Usuario implements Serializable {
     // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 50)
+    @Size(min = 1, max = 70)
     @Column(name = "email")
     private String email;
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 15)
+    @Size(min = 1, max = 20)
     @Column(name = "celular")
     private String celular;
     @Basic(optional = false)
