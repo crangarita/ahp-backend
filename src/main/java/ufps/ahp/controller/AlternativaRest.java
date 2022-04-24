b31b42042eea432d9c5096508fc8b6f7d052db12
Santiago Serrano <santiagoandressp@ufps.edu.co>
2022-04-24 00:00:42 -0500

    Prueba rest

--- src/main/java/ufps/ahp/controller/AlternativaRest.java
+++ src/main/java/ufps/ahp/controller/AlternativaRest.java
@@ -4,10 +4,7 @@ package ufps.ahp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ufps.ahp.model.Alternativa;
 import ufps.ahp.services.AlternativaService;
 
@@ -26,5 +23,9 @@ public class AlternativaRest {
         return ResponseEntity.ok(alternativaService.listar());
     }
 
+    @GetMapping("/{idAlternativa}")
+    public ResponseEntity<?> encontrarAlternativa(@PathVariable int idAlternativa){
+        return ResponseEntity.ok(alternativaService.buscar(idAlternativa));
+    }
 
 }
